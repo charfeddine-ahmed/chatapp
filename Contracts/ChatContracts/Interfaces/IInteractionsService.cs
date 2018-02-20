6b61e26a86086ce51d9805d7fe2c89761379428b
Ahmed Charfeddine <ACharfeddine@se.linedata.com>
2018-02-20 14:41:29 +0100

    folder structure and initial seeds

---
+++ Contracts/ChatContracts/Interfaces/IInteractionsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatContracts.Interfaces
+{
+    interface IInteractionsService
+    {
+        bool SendDirectMessage(string destination, string message);
+
+        void JoinRoom(string roomName);
+
+        void LeaveRoom(string roomName);
+
+        void SendBroadcastMessage(string roomName, string message);
+    }
+}
---
+++ Contracts/ChatContracts/Interfaces/IQueryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ChatContracts.Data;
+
+namespace ChatContracts.Interfaces
+{
+    interface IQueryService
+    {
+        List<Room> GetRoomsList();
+
+        List<Participant> GetParticipantsList();
+    }
+}
